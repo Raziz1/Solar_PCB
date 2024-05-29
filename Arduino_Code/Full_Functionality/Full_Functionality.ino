b19c43d5d267d38ead04ba20e157d03c23d38e6e
Rahim Aziz <rahimaziz31@gmail.com>
2024-05-28 21:31:59 -0400

    Updated Full_Functionality Script
    Now utilizing Narcoleptic library for low power modes

--- Arduino_Code/Full_Functionality/Full_Functionality.ino
+++ Arduino_Code/Full_Functionality/Full_Functionality.ino
@@ -23,6 +23,9 @@ struct DHT11DataObject {
   int index;
 };
 
+#define LED 13
+#define DEBUGMODE 0
+
 // Create an instance of the DHT11 class.
 // - For Arduino: Connect the sensor to Digital I/O Pin 2.
 DHT11 dht11(2);
@@ -37,36 +40,36 @@ void setup() {
 
   // Uncomment the line below to set a custom delay between sensor readings (in milliseconds).
   // dht11.setDelay(500); // Set this to the desired delay. Default is 500ms.
+
+  // Low power mode setup
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,LOW);
+
+  disableAllPeriph();
+  // Another tweaks to lower the power consumption
+  ADCSRA &= ~(1<<ADEN); //Disable ADC
+  ACSR = (1<<ACD); //Disable the analog comparator
+
+  // ATmega48/88/168/328
+  DIDR0 = B00111111; //Disable digital input buffers on all ADC0-ADC5 pins
+  DIDR1 = (1<<AIN1D)|(1<<AIN0D); //Disable digital input buffer on AIN1/0
 }
 
 // ===============================
 // Main Loop
 // ===============================
 void loop() {
-  if (Serial.available() > 0) 
+  if(DEBUGMODE)
   {
-    switch (Serial.read())
-    {
-    case 'w':
-      putEEPROM(); 
-      break;
-
-    case 'r':
-      getEEPROM();
-      break;
-
-    case 'c':
-      clearEEPROM();
-      break;
-
-    case 't':
-      writeTEMP();
-      break;
-
-    default:
-      Serial.println("Not valid input!");
-      break;
-    }
+    debugMode();
+  }
+  else
+  {
+    Narcoleptic.delay(10000); //uncalibrated delay for 10s
+    Narcoleptic.enableSerial();
+    readTEMP();
+    Serial.flush();
+    Narcoleptic.disableSerial();
   }
 }
 
@@ -178,7 +181,125 @@ void writeTEMP()
         EEPROM.put(writeTempAddress, tempReading);
         writeTempAddress += sizeof(DHT11DataObject);       
       }
+      else
+      {
+        Serial.println("EEPROM out of range!");
+      }
+    } 
+    else 
+    {
+        Serial.println(DHT11::getErrorString(result));
+    }
+}
+
+/**
+ * Reads environmental data from the DHT11
+ * 
+ * @param 'N/A'
+ * @return N/A
+ * @throws N/A
+ * 
+ */
+void readTEMP()
+{
+    int temperature = 0;
+    int humidity = 0;
+
+    // Attempt to read the temperature and humidity values from the DHT11 sensor.
+    int result = dht11.readTemperatureHumidity(temperature, humidity);
+
+    // Check the results of the readings.
+    if (result == 0) {
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.print(" °C\tHumidity: ");
+      Serial.print(humidity);
+      Serial.println(" %");
     } else {
         Serial.println(DHT11::getErrorString(result));
     }
+}
+
+/**
+ * Utilizes the Narcoleptic library to disable all peripherals
+ * 
+ * @param 'N/A'
+ * @return N/A
+ * @throws N/A
+ * 
+ */
+void disableAllPeriph()
+{
+  Narcoleptic.disableWire();
+  Narcoleptic.disableTimer1();
+  Narcoleptic.disableTimer2();
+  Narcoleptic.disableTimer3();
+  Narcoleptic.disableTimer4();
+  Narcoleptic.disableSerial();
+  Narcoleptic.disableADC();
+  Narcoleptic.disableSPI();
+  Narcoleptic.disableTouch();
+}
+
+/**
+ * Utilizes the Narcoleptic library to enable all peripherals
+ * 
+ * @param 'N/A'
+ * @return N/A
+ * @throws N/A
+ * 
+ */
+void enableAllPeriph()
+{
+  Narcoleptic.enableWire();
+  Narcoleptic.enableTimer1();
+  Narcoleptic.enableTimer2();
+  Narcoleptic.enableTimer3();
+  Narcoleptic.enableTimer4();
+  Narcoleptic.enableSerial();
+  Narcoleptic.enableADC();
+  Narcoleptic.enableSPI();
+  Narcoleptic.enableTouch();
+}
+
+/**
+ * Takes a keyboard input and runs the appropriate function. 
+ * This function is primarily used for debugging
+ * 
+ * @param 'N/A'
+ * @return N/A
+ * @throws N/A
+ * 
+ */
+void debugMode()
+{
+  if (Serial.available() > 0) 
+  {
+    switch (Serial.read())
+    {
+    case 'w':
+      putEEPROM(); 
+      break;
+
+    case 'r':
+      getEEPROM();
+      break;
+
+    case 'c':
+      clearEEPROM();
+      break;
+
+    case 'a':
+      readTEMP();
+      break;
+
+    case 't':
+      writeTEMP();
+      break;
+
+    default:
+      Serial.println("Not valid input!");
+      break;
+    }
+  }
 }
